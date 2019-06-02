9a899c71d85d09faddbf9d0f3bdefd2631d90b48
pd <pd.shahsafi@gmail.com>
2019-06-02 11:40:43 +0430

    1 done

--- src/instance/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-DATABASE_BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or \
-    "sqlite:///" + os.path.join(DATABASE_BASE_DIR, "db")
-SQLALCHEMY_TRACK_MODIFICATIONS=False
